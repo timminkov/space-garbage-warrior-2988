31cc8c84f9b9ee269c5b60c9247aa0e3ad8c6112
Tim Minkov <timothyminkov@gmail.com>
2014-09-13 13:26:49 -0700

    Working blackholes

--- game/states/play.js
+++ game/states/play.js
@@ -4,23 +4,34 @@
   Play.prototype = {
     create: function() {
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
-      this.sprite.inputEnabled = true;
-      
-      this.game.physics.arcade.enable(this.sprite);
-      this.sprite.body.collideWorldBounds = true;
-      this.sprite.body.bounce.setTo(1,1);
-      this.sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-      this.sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
-
-      this.sprite.events.onInputDown.add(this.clickListener, this);
+      this.timer = this.game.time.create(true);
+      this.timer.start();
+      this.timer.loop(1000, this.createBall, this);
     },
+
     update: function() {
-
+      if (this.game.input.activePointer.isDown) {
+        this.createBlackHole();
+      }
     },
-    clickListener: function() {
-      this.game.state.start('gameover');
+
+    createBall: function() {
+      var x = Math.floor((Math.random() * 600) + 1)
+      var y = Math.floor((Math.random() * 800) + 1)
+
+      var ball = this.game.add.sprite(x, y, 'ball');
+      this.game.physics.arcade.enable(ball);
+      ball.body.velocity.x = Math.floor((Math.random() * 10) + 1);
+      ball.body.velocity.y = Math.floor((Math.random() * 10) + 1);
+    },
+
+    createBlackHole: function() {
+      this.blackHole = this.game.add.sprite(this.game.input.activePointer.worldX-32, this.game.input.activePointer.worldY-32, 'blackhole');
+      this.blackHole.animations.add('pulse');
+      this.blackHole.animations.play('pulse', 2, true);
+
+      this.game.physics.arcade.enable(this.blackHole);
     }
   };
-  
+
   module.exports = Play;
