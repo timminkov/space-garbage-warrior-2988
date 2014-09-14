0a3f467edf70d5c3c3ae4c02a5a287b090f22dd4
Tim Minkov <timothyminkov@gmail.com>
2014-09-13 22:43:44 -0700

    WIP

--- game/states/play.js
+++ game/states/play.js
@@ -89,6 +89,18 @@
       this.player.changeHealth(trash.object.damageAmount);
     },
 
+    updateTrash: function(trash) {
+      if(trash.x < -18 || trash.y < -30 || trash.y > 830) {
+        if (trash.alive) {
+          trash.kill();
+        }
+      }
+    },
+
+    damagePlayer: function(trash) {
+      this.player.changeHealth(trash.object.damageAmount);
+    },
+
     createTrash: function() {
       var trash = new Trash(this.game, this.player).create();
 
